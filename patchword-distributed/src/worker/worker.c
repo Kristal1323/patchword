84f248e937d6ceb540467dd7c760c361ff1fa9e5
Kristal1323 <kristalmaung@gmail.com>
2025-11-25 20:08:57 +1000

    distributed: Add worker startup handshake and WORKER_READY emission

--- patchword-distributed/src/worker/worker.c
+++ patchword-distributed/src/worker/worker.c
@@ -1,10 +1,24 @@
 #include "worker.h"
 #include "protocol.h"
 #include "util.h"
+#include "worker_logic.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int worker_main(int argc, char **argv) {
-    log_worker(0, "Worker startup (args=%d)", argc);
+    (void)argc; (void)argv;
 
-    // TODO: implement later
-    return 0;
+    log_worker(getpid(), "Worker process started");
+
+    // Send WORKER_READY to coordinator
+    char line[MAX_MSG];
+    Message out;
+    out.type = MSG_WORKER_READY;
+    format_message(line, sizeof(line), &out);
+    safe_writeline(STDOUT_FILENO, line);
+
+    // Main loop: read messages from coordinator
 }
